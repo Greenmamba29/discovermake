72e7d13b6ddfa8d45491dc8f906b45c57c918800
Greenmamba29 <mcdonald@lithiumbuy.com>
2026-02-12 19:26:12 -0500

    fix: explicit turbopack root and remote patterns to optimize build

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.pravatar.cc',
-            },
-            {
-                protocol: 'https',
-                hostname: 'plus.unsplash.com',
-            }
+            { protocol: 'https', hostname: 'images.unsplash.com' },
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+            { protocol: 'https', hostname: 'i.pravatar.cc' },
+            { protocol: 'https', hostname: 'plus.unsplash.com' }
         ]
-    }
+    },
+    turbopack: {
+        root: path.resolve('.'),
+    },
 };
 
 module.exports = nextConfig;
